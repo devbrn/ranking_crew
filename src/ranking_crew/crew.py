96fe74972cd60ce6c52273f36d3a627d7558fdd7
devbrn <brunosilva.consultoria@gmail.com>
2024-08-05 16:02:30 -0300

    Update crew.py

--- src/ranking_crew/crew.py
+++ src/ranking_crew/crew.py
@@ -3,18 +3,9 @@
 from crewai import Agent, Task, Crew, Process
 from tools.custom_tool import example_tool
 
-search_tool = SerperDevTool()
-
 def run_crew():
     # Criando um agente pesquisador com uma ferramenta de pesquisa
-    researcher = Agent(
-        role='Researcher',
-        goal='Conduct research on the given job description',
-        backstory='You are skilled at finding the right candidates by exploring various online resources.',
-        tools=[search_tool]
-    )
-
-    # Definindo a tarefa de pesquisa
+    
     research_task = Task(
         description='Conduct research to find potential candidates for the specified job.',
         expected_output='A list of potential candidates with their profiles.',
